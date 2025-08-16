c45584a11cb7cf753f4e73aad23bf7835a2b8737
Nikhil Singhal <nikhilsinghal000@gmail..com>
2025-08-16 15:58:15 +0530

    db connection via env

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,10 +1,20 @@
-import pkg from "pg";
-const { Pool } = pkg;
+// import pkg from "pg";
+// const { Pool } = pkg;
 
-export const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "approval_db",
-  password: "root",
-  port: 5432,
-});
+// export const pool = new Pool({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "approval_db",
+//   password: "root",
+//   port: 5432,
+// });
+
+import pg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
+
+const config = {
+  connectionString: process.env.DATABASE_URL,
+};
+
+export const pool = new pg.Pool(config);
